e33a721d4d02db1de1692ccf135f0a53129b6d64
Daryl <17547337+dboggs95@users.noreply.github.com>
2022-02-06 13:22:32 -0500

    Saving failed attempt to fix demo with module-info.

---
+++ workbenchfx-demo/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module com.dlsc.workbenchfx.demo {
+
+	requires javafx.controls;
+	requires de.jensd.fx.fontawesomefx.materialdesignicons;
+	requires com.dlsc.workbenchfx.core;
+	requires slf4j.api;
+	requires de.jensd.fx.fontawesomefx.fontawesome;
+	requires javafx.fxml;
+	requires GMapsFX;
+	requires com.google.common;
+	requires javafx.web;
+	requires org.controlsfx.controls;
+	requires com.dlsc.formsfx;
+	requires com.calendarfx.view;
+	requires com.dlsc.preferencesfx;
+
+}
